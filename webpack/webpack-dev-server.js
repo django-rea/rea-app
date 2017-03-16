4ee6ffa91bb1af4b58ca9c40e73f3baac46a0cd1
pospi <pospi@spadgos.com>
2017-03-16 18:02:17 +0200

    compilation debug and source cocntext dir

--- webpack/webpack-dev-server.js
+++ webpack/webpack-dev-server.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+const path = require('path')
 const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const config = require('./webpack.config')
@@ -10,7 +11,28 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   host: ip,
-  stats: false,
+  stats: {
+    // @see https://github.com/webpack/docs/wiki/node.js-api#stats
+    context: path.resolve(__dirname, '../'),
+    hash: false,
+    version: false,
+    timings: true,
+    assets: true,
+    entrypoints: true,
+    chunks: true,
+    chunkModules: true,
+    chunkOrigins: true,
+    modules: false,
+    cached: false,
+    children: true,
+    warnings: true,
+    errorDetails: true,
+    reasons: false,
+    usedExports: true,
+    providedExports: true,
+    colors: true,
+    source: false,
+  },
   historyApiFallback: true,
   contentBase: 'public',
   compress: true,
