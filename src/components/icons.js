6d949b81411881508df6ae5c44904838e8c94306
pospi <pospi@spadgos.com>
2017-03-19 19:33:55 +0200

    add menu icon

--- src/components/icons.js
+++ src/components/icons.js
@@ -13,6 +13,7 @@ import { browser } from '../config'
 
 const nullServerComponent = () => (<span />)
 
+export const Menu = !browser ? nullServerComponent : require('material-design-icons/navigation/svg/production/ic_menu_24px.svg')
 // Checkbox: require('material-design-icons/toggle/svg/production/ic_check_box_24px.svg'),
 export const Checkbox = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_check_circle_24px.svg')
 
