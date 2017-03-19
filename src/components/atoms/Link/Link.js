367afd69dcee5d418ead1b41e4b9330b39bf0bc9
pospi <pospi@spadgos.com>
2017-03-19 20:28:15 +0200

    redo link from react-router base

--- src/components/atoms/Link/Link.js
+++ src/components/atoms/Link/Link.js
@@ -8,17 +8,26 @@
  * @flow
  */
 
+import type { Element } from 'react'
+
 import React from 'react'
-import { Link as BaseLink } from 'react-toolbox/lib/link'
+import { Link as BaseLink } from 'react-router'
 
 import styles from './Link.css'
 
 type Props = {
   className: string,
+  activeClassName?: string,
+  children: Element<*>,
 };
 
-const Link = ({ className, ...props }: Props) => {
-  return <BaseLink {...props} className={styles[className]} />
-}
+const Link = ({ className, activeClassName, children, ...props }: Props) => (
+  <BaseLink {...props}
+    className={styles[className]}
+    activeClassName={activeClassName ? styles[activeClassName] : null}
+  >
+    {children}
+  </BaseLink>
+)
 
 export default Link
