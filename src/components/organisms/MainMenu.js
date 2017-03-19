59d0c67d8844c53d2b0fa29cff8ff10d0e36b780
pospi <pospi@spadgos.com>
2017-03-19 20:40:44 +0200

    main menu links

--- src/components/organisms/MainMenu.js
+++ src/components/organisms/MainMenu.js
@@ -7,10 +7,34 @@
  * @flow
  */
 
-import React from 'react'
+import type { Router } from 'react-router'
 
-const MainMenu = () => (
-  <h1 style={{ color: '#F0F', fontWeight: 'bold', textAlign: 'center' }}>Coming soon...</h1>
-)
+import React from 'react'
+import { withRouter } from 'react-router'
+
+import { IconMenu, MenuItem, MenuDivider } from 'react-toolbox/lib/menu'
+import { Checkbox, Person, Group, Exit } from 'components/icons'
+
+type Props = {
+  router: Router,
+};
+
+const routeTo = (router) => (url: string) => () => {
+  router.push(url)
+}
+
+const MainMenu = withRouter(({ router }: Props) => {
+  const navTo = routeTo(router)
+
+  return (
+    <div>
+      <MenuItem onClick={navTo('/tasks')} icon={<Checkbox style={{ verticalAlign: 'top' }} />} caption='Tasks' />
+      <MenuItem onClick={navTo('/projects')} icon={<Group style={{ verticalAlign: 'top' }} />} caption='Projects' />
+      <MenuItem onClick={navTo('/profile')} icon={<Person style={{ verticalAlign: 'top' }} />} caption='Profile' />
+      <MenuDivider />
+      <MenuItem onClick={navTo('/signout')} icon={<Exit style={{ verticalAlign: 'top' }} />} caption='Sign out' />
+    </div>
+  )
+})
 
 export default MainMenu
