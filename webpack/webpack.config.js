1a59030215028f206a2589aee43281cf9c54f284
pospi <pospi@spadgos.com>
2017-03-28 17:57:33 +0300

    allow PostCSS to handle single-line comments and strip comments during compilation

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -38,17 +38,22 @@ const config = {
       test: /\.css$/,
       options: {
         postcss: (webpack) => {
-          return [
-            require("postcss-import")({ addDependencyTo: webpack }),
-            require("postcss-url")(),
-            require("postcss-cssnext")(),
-            // add your "plugins" here
-            // ...
-            // and if you want to compress,
-            // just use css-loader option that already use cssnano under the hood
-            require("postcss-browser-reporter")(),
-            require("postcss-reporter")(),
-          ]
+          return {
+            plugins: [
+              require("postcss-import")({ addDependencyTo: webpack }),
+              require("postcss-url")(),
+              require("postcss-cssnext")(),
+              // add your "plugins" here
+              // ...
+              require('postcss-strip-inline-comments')(),
+              require('postcss-discard-comments')(),
+              // and if you want to compress,
+              // just use css-loader option that already use cssnano under the hood
+              require("postcss-browser-reporter")(),
+              require("postcss-reporter")(),
+            ],
+            syntax: require('postcss-scss'),
+          }
         },
       },
     }),
