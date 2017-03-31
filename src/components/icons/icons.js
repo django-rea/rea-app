037f91db13ceffb3f1252e2350e0a38592b695c7
pospi <pospi@spadgos.com>
2017-03-31 11:12:21 +0300

    add todo & fix icon imports

--- src/components/icons/icons.js
+++ src/components/icons/icons.js
@@ -1,6 +1,8 @@
 /**
  * SVG icon components
  *
+ * :TODO: remove `nullServerComponent` workaround and make this work serverside
+ *
  * @package: OCP app
  * @author:  pospi <pospi@spadgos.com>
  * @since:   2017-03-19
@@ -13,16 +15,16 @@ import { browser } from '../../config'
 
 const nullServerComponent = () => (<span />)
 
-export const FreedomCoopLogo = !browser ? nullServerComponent : require('./FC-logo-only.svg')
+export const FreedomCoopLogo = !browser ? nullServerComponent : require('./FC-logo-only.svg').default
 
-export const Menu = !browser ? nullServerComponent : require('material-design-icons/navigation/svg/production/ic_menu_24px.svg')
-// Checkbox: require('material-design-icons/toggle/svg/production/ic_check_box_24px.svg'),
-export const Checkbox = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_check_circle_24px.svg')
-export const Exit = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_exit_to_app_24px.svg')
+export const Menu = !browser ? nullServerComponent : require('material-design-icons/navigation/svg/production/ic_menu_24px.svg').default
+// Checkbox: require('material-design-icons/toggle/svg/production/ic_check_box_24px.svg').default
+export const Checkbox = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_check_circle_24px.svg').default
+export const Exit = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_exit_to_app_24px.svg').default
 
-export const Person = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_account_circle_24px.svg')
+export const Person = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_account_circle_24px.svg').default
 
-export const Group = !browser ? nullServerComponent : require('material-design-icons/social/svg/production/ic_group_24px.svg')
-export const GroupAdd = !browser ? nullServerComponent : require('material-design-icons/social/svg/production/ic_group_add_24px.svg')
+export const Group = !browser ? nullServerComponent : require('material-design-icons/social/svg/production/ic_group_24px.svg').default
+export const GroupAdd = !browser ? nullServerComponent : require('material-design-icons/social/svg/production/ic_group_add_24px.svg').default
 
-export const Bank = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_account_balance_24px.svg')
+export const Bank = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_account_balance_24px.svg').default
