8fae8ee0e42fa88f8b02ba71dc492b03bba12c69
pospi <pospi@spadgos.com>
2017-04-05 12:42:17 +0300

    added a form input wrapper (think inputs as displayed on 'normal' forms). Needs label or some such.

---
+++ src/components/molecules/FormInput/FormInput.js
@@ -0,0 +1,51 @@
+/**
+ * Like an Input component, but with extra stuff surrounding it for displaying in a form
+ *
+ * @package: REA app
+ * @author:  pospi <pospi@spadgos.com>
+ * @since:   2017-04-05
+ * @flow
+ */
+
+import React from 'react'
+
+import Input from 'components/atoms/Input'
+
+export type FieldProps = {
+  // bespoke
+  placeholder: ?string,
+  // added by redux-form
+  input: {
+    checked: ?boolean,
+    name: string,
+    onBlur: (any) => void,
+    onChange: (any) => void,
+    onDragStart: (any) => void,
+    onDrop: (any) => void,
+    onFocus: (any) => void,
+    value: any,
+  },
+  meta: {
+    active: boolean,
+    autofilled: boolean,
+    asyncValidating: boolean,
+    dirty: boolean,
+    dispatch: Function,
+    error: ?string,
+    form: string,
+    invalid: boolean,
+    pristine: boolean,
+    submitting: boolean,
+    submitFailed: boolean,
+    touched: boolean,
+    valid: boolean,
+    visited: boolean,
+    warning: ?string,
+  },
+};
+
+const FormInput = ({ input, meta, ...props }: FieldProps) => (
+  <Input {...props} {...input} />
+)
+
+export default FormInput
