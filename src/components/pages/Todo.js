5ba6713b73c8c6a6c8227d8a3c75c7371a00f4d7
pospi <pospi@spadgos.com>
2017-04-24 22:06:48 +1000

    update todo page stub with username when logged in

--- src/components/pages/Todo.js
+++ src/components/pages/Todo.js
@@ -11,9 +11,29 @@ import React from 'react'
 import T from 'i18n-react'
 import PageTemplate from 'components/templates/PageTemplate'
 
+import CurrentUser from 'containers/user/CurrentUser'
+
+type UserProps = {
+  data: {
+    viewer: {
+      agent: {
+        name: string,
+      },
+    },
+    loading: bool,
+    error: ?Error,
+  },
+};
+
+/* eslint no-nested-ternary: 0 */
+const UsernameDisplay = CurrentUser(({ data: { viewer, loading, error } }: UserProps) => (
+  loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error</p> : <p>Hello, {viewer ? viewer.agent.name : 'nobody'}.</p>)
+))
+
 const Todo = () => (
   <PageTemplate>
     <h1 style={{ color: '#F0F', fontWeight: 'bold', textAlign: 'center' }}><T.text text="todo_text" /></h1>
+    <UsernameDisplay />
   </PageTemplate>
 )
 
