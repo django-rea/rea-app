90ac142111a1e3791a4196beaf81d2c5c9790942
pospi <pospi@spadgos.com>
2017-05-01 23:00:37 +1000

    add js-yaml to make test runner aware of how to load YAML files

--- packages/test-helpers/bootstrap.js
+++ packages/test-helpers/bootstrap.js
@@ -7,6 +7,8 @@
  * @flow
  */
 
+var yaml = require('js-yaml')
+
 require.extensions['.scss'] = function(m, filename) {}
 require.extensions['.sass'] = function(m, filename) {}
 require.extensions['.less'] = function(m, filename) {}
@@ -15,3 +17,7 @@ require.extensions['.css'] = function(m, filename) {}
 require.extensions['.svg'] = function(m, filename) {}
 require.extensions['.png'] = function(m, filename) {}
 require.extensions['.jpg'] = function(m, filename) {}
+
+require.extensions['.yaml'] = function(m, filename) {
+  return yaml.safeLoad(filename)
+}
