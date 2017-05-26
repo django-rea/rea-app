8fc945a8712a7c993c30da971195315656545c37
pospi <pospi@spadgos.com>
2017-05-26 22:59:50 +1000

    define some custom typings to account for transpiler work on node internals

--- custom.d.ts
+++ custom.d.ts
@@ -1,4 +1,24 @@
+// custom module inclue types
 declare module '*.svg' {
   const content: any
   export default content
+}
+
+// custom handling of require mechanisms & node internals by Webpack
+declare interface NodeModule {
+  hot: {
+    accept: (path: string, onReload: Function) => void
+  },
+}
+
+declare interface NodeRequire {
+  context: Function,
+}
+
+declare interface Global {
+  webpackIsomorphicTools: any,
+}
+
+declare interface Window {
+  __INITIAL_STATE__: string,
 }
