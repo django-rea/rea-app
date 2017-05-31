0e956a10b3ec03ea28ce30947c1c9fe7c6265ebb
pospi <pospi@spadgos.com>
2017-06-01 09:09:51 +1000

    add global module whitelist for SCSS

---
+++ packages/app/webpack/global-scss-modules-whitelist.js
@@ -0,0 +1,16 @@
+/**
+ * All source files matching paths declared within this module will be interpreted with global
+ * names when being parsed by PostCSS.
+ *
+ * You should ensure that the global files which you declare your mixins etc in are specified here,
+ * otherwise the names won't match by the time PostCSS encounters them and you will get "Undefined
+ * mixin" errors in your code!
+ */
+
+module.exports = [
+  /\/node_modules\//,               // <-- this is the default
+  /(ui-|@vflows\/)views\/base\.scss$/,
+  /(ui-|@vflows\/)views\/mixin\.scss$/,
+  /(ui-|@vflows\/)views\/typography\.scss$/,
+  /(ui-|@vflows\/)views\/variables\.scss$/,
+];
