ecbcd4f265bee9ca524c17edd6cfd557e010c9da
pospi <pospi@spadgos.com>
2017-05-31 16:18:10 +1000

    convert aside to simple functional component

--- packages/ui-views/organisms/aside/aside.tsx
+++ packages/ui-views/organisms/aside/aside.tsx
@@ -1,38 +1,35 @@
-import React, {Component} from 'react'
+import * as React from 'react'
+import { SFC } from 'react'
 
-class Aside extends Component {
-    render () {
-        return (
-            <aside id='aside'>
-              <span className='aside_logo' />
-              <ul className='aside_list'>
-                  <li className='list_item'>
-                      <a className='item_link'>
-                          <span className='link_image' />
-                      </a>
-                  </li>
-                  <li className='list_item active'>
-                      <a className='item_link'>
-                          <span className='link_image' />
-                      </a>
-                  </li>
-                  <li className='list_item'>
-                      <a className='item_link'>
-                          <span className='link_image' />
-                      </a>
-                  </li>
-                  <li className='list_item'>
-                      <a className='item_link'>
-                          <span className='link_image' />
-                      </a>
-                  </li>
-              </ul>
-              <div className='aside_bottom'>
-                  <span className='icon-plus' />
-              </div>
-            </aside>
-        )
-    }
-}
+const Aside: SFC<{}> = () => (
+  <aside id='aside'>
+    <span className='aside_logo' />
+    <ul className='aside_list'>
+        <li className='list_item'>
+            <a className='item_link'>
+                <span className='link_image' />
+            </a>
+        </li>
+        <li className='list_item active'>
+            <a className='item_link'>
+                <span className='link_image' />
+            </a>
+        </li>
+        <li className='list_item'>
+            <a className='item_link'>
+                <span className='link_image' />
+            </a>
+        </li>
+        <li className='list_item'>
+            <a className='item_link'>
+                <span className='link_image' />
+            </a>
+        </li>
+    </ul>
+    <div className='aside_bottom'>
+        <span className='icon-plus' />
+    </div>
+  </aside>
+)
 
 export default Aside
