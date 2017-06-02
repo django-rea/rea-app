a3aec184110347c84d10e7c5c2c8dc79c6fa006e
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-02 15:49:34 +0200

    updated themeable projDescription component

---
+++ packages/ui-views/organisms/projectDescription/index.tsx
@@ -0,0 +1,5 @@
+import themed from '../../themed'
+import Component from './projectDescription'
+const styles = require('./index.scss')
+
+export default themed(Component, styles)
--- packages/ui-views/organisms/projectDescription/projectDescription.tsx
+++ packages/ui-views/organisms/projectDescription/projectDescription.tsx
@@ -1,12 +1,17 @@
-import React, {Component} from 'react'
+import * as React from 'react'
+import * as themeable from 'react-themeable'
+import { SFC } from 'react'
 
-class ProjectDescription extends Component {
+interface Props {
+  theme: Object
+}
 
-  render () {
+const ProjectDescription: SFC<Props> = ({ theme }) => {
+    let currentTheme = themeable(theme)
     return (
-     <section id='projectDescription'>
-        <h4 className='description_title'>Project description</h4>
-        <div className='description'>Everybody that has ever been to a meeting, can recall the all familiar “passing” of the business cards. This powerfull marketing tool is often used matter of factly, and we have to wonder how often does the recipient of your card hold on to it, and use your business information positively?
+     <section {...currentTheme(1, 'projectDescription')} >
+        <h4 {...currentTheme(2, 'description_title')} >Project description</h4>
+        <div {...currentTheme(3, 'description')} >Everybody that has ever been to a meeting, can recall the all familiar “passing” of the business cards. This powerfull marketing tool is often used matter of factly, and we have to wonder how often does the recipient of your card hold on to it, and use your business information positively?
 In a world where first impressions count and business is becoming more competitive, it would be foolish to go with the crowd.</div>
     </section>
     )
