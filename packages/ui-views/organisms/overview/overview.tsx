f83e4c26abb0361d8c00e0a220bdd391a1d519ff
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-02 17:02:56 +0200

    updated themeable overview component

--- packages/ui-views/organisms/overview/overview.tsx
+++ packages/ui-views/organisms/overview/overview.tsx
@@ -1,33 +1,38 @@
-import React, {Component} from 'react'
+import * as React from 'react'
+import * as themeable from 'react-themeable'
+import { SFC } from 'react'
 
-class Overview extends Component {
+interface Props {
+  theme: Object
+}
 
-  render () {
+const Overview: SFC<Props> = ({ theme }) => {
+    let currentTheme = themeable(theme)
     return (
-     <section id='overview'>
-         <div className='row'>
-             <div className='medium-3 columns'>
-                 <div className='overview_item'>
-                    <h2 className='item_qty'>56</h2>
-                    <h4 className='item_title'>Processes</h4>
+     <section {...currentTheme(0, 'overview')}>
+         <div {...currentTheme(1, 'row')}>
+             <div {...currentTheme(2, 'medium-3', 'columns')}>
+                 <div {...currentTheme(3, 'overview_item')}>
+                    <h2 {...currentTheme(4, 'item_qty')}>56</h2>
+                    <h4 {...currentTheme(5, 'item_title')}>Processes</h4>
                  </div>
              </div>
-             <div className='medium-3 columns'>
-                 <div className='overview_item'>
-                    <h2 className='item_qty'>156</h2>
-                    <h4 className='item_title'>Members</h4>
+             <div {...currentTheme(6, 'medium-3', 'columns')}>
+                 <div {...currentTheme(7, 'overview_item')}>
+                    <h2 {...currentTheme(8, 'item_qty')}>156</h2>
+                    <h4 {...currentTheme(9, 'item_title')}>Members</h4>
                  </div>
              </div>
-             <div className='medium-3 columns'>
-                 <div className='overview_item'>
-                    <h2 className='item_qty'>202</h2>
-                    <h4 className='item_title'>Inventory Items</h4>
+             <div {...currentTheme(10, 'medium-3', 'columns')}>
+                 <div {...currentTheme(11, 'overview_item')}>
+                    <h2 {...currentTheme(12, 'item_qty')}>202</h2>
+                    <h4 {...currentTheme(13, 'item_title')}>Inventory Items</h4>
                  </div>
              </div>
-             <div className='medium-3 columns'>
-                 <div className='overview_item'>
-                    <h2 className='item_qty'>3</h2>
-                    <h4 className='item_title'>Recipes</h4>
+             <div {...currentTheme(14, 'medium-3', 'columns')}>
+                 <div {...currentTheme(15, 'overview_item')}>
+                    <h2 {...currentTheme(16, 'item_qty')}>3</h2>
+                    <h4 {...currentTheme(17, 'item_title')}>Recipes</h4>
                  </div>
              </div>
          </div>
