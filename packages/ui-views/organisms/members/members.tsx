1acea13f930d3318f5c06a0356148809ebed4004
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-02 21:04:13 +0200

    update members themeable component

--- packages/ui-views/organisms/members/members.tsx
+++ packages/ui-views/organisms/members/members.tsx
@@ -1,53 +1,57 @@
-import React, {Component} from 'react'
+import * as React from 'react'
+import * as themeable from 'react-themeable'
+import { SFC } from 'react'
 
-class Members extends Component {
+interface Props {
+  theme: Object
+}
 
-  render () {
+const Members: SFC<Props>  = ({ theme }) => {
+    let currentTheme = themeable(theme)
     return (
-     <section id='members'>
-        <h4 className='members_title'>Members</h4>    
-        <ul className='members_list'>
-            <li className='list_item'>
-                <a className='item_link'>
-                    <span className='link_image'></span>
+        <section {...currentTheme(0, 'members')}>
+        <h4 {...currentTheme(1, 'members_title')}>Members</h4>    
+        <ul {...currentTheme(2, 'members_list')}>
+            <li {...currentTheme(3, 'list_item')}>
+                <a {...currentTheme(4, 'item_link')}>
+                    <span {...currentTheme(5, 'link_image')}></span>
                 </a>
             </li>
-            <li className='list_item'>
-                <a className='item_link'>
-                    <span className='link_image'></span>
+            <li {...currentTheme(6, 'list_item')}>
+                <a {...currentTheme(7, 'item_link')}>
+                        <span {...currentTheme(8, 'link_image')}></span>
                 </a>
             </li>
-            <li className='list_item'>
-                <a className='item_link'>
-                    <span className='link_image'></span>
+            <li {...currentTheme(9, 'list_item')}>
+                <a {...currentTheme(10, 'item_link')}>
+                    <span {...currentTheme(11, 'link_image')}></span>
                 </a>
             </li>
-            <li className='list_item'>
-                <a className='item_link'>
-                    <span className='link_image'></span>
+            <li {...currentTheme(12, 'list_item')}>
+                <a {...currentTheme(13, 'item_link')}>
+                    <span {...currentTheme(14, 'link_image')}></span>
                 </a>
             </li>
-            <li className='list_item'>
-                <a className='item_link'>
-                    <span className='link_image'></span>
+            <li {...currentTheme(15, 'list_item')}>
+                <a {...currentTheme(16, 'item_link')}>
+                    <span {...currentTheme(17, 'link_image')}></span>
                 </a>
             </li>
-            <li className='list_item'>
-                <a className='item_link'>
-                    <span className='link_image'></span>
+            <li {...currentTheme(18, 'list_item')}>
+                <a {...currentTheme(19, 'item_link')}>
+                    <span {...currentTheme(20, 'link_image')}></span>
                 </a>
             </li>
-            <li className='list_item'>
-                <a className='item_link'>
-                    <span className='link_image'></span>
+            <li {...currentTheme(21, 'list_item')}>
+                <a {...currentTheme(22, 'item_link')}>
+                    <span {...currentTheme(23, 'link_image')}></span>
                 </a>
             </li>
 
         </ul>
-        <a className='members_link'>See All</a>
-     </section>
+        <a {...currentTheme(24, 'members_link')}>See All</a>
+        </section>
     )
-  }
 }
 
 export default Members
