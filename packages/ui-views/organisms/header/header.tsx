74cdd652d9e69e601cd53bf65b1f473a8ece74f3
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-02 14:56:25 +0200

    fix themeable component

--- packages/ui-views/organisms/header/header.tsx
+++ packages/ui-views/organisms/header/header.tsx
@@ -10,26 +10,29 @@ interface Props {
   theme: Object
 }
 
-const Header: SFC<Props> = ({ theme }) => (
-    <header {...themeable(theme)(1, 'main_header')} >
-        <h2  {...themeable(theme)(2, 'header_title')}>FreedomCoop</h2>
-        <div className='header_menu'>
-            <div className='menu_search'>
-                <input className='search input' placeholder='Search' />
-                <span className='icon-magnifying-glass' />
+const Header: SFC<Props> = ({ theme }) => {
+    let currentTheme = themeable(theme)
+    return (
+        <header {...currentTheme(1, 'main_header')} >
+            <h2  {...currentTheme(2, 'header_title')}>FreedomCoop</h2>
+            <div {...currentTheme(3, 'header_menu')} >
+                <div {...currentTheme(4, 'menu_search')} >
+                    <input {...currentTheme(5, 'search input')} placeholder='Search' />
+                    <span {...currentTheme(6, 'icon-magnifying-glass')} />
+                </div>
+                <ul {...currentTheme(7, 'menu_list')} >
+                    <li {...currentTheme(8, 'list_item')} ><a {...currentTheme(9, 'item_link')} >menu item 1</a></li>
+                    <li {...currentTheme(10, 'list_item')} ><a {...currentTheme(11, 'item_link')} >menu item 2</a></li>
+                    <li {...currentTheme(12, 'list_item')} ><a {...currentTheme(13, 'item_link')} >menu item 3</a></li>
+                </ul>
+                <div {...currentTheme(14, 'menu_profile')} >
+                    <h4>Pospigos</h4>
+                    <div {...currentTheme(15, 'profile_image')}  />
+                    <span {...currentTheme(16, 'icon-dots-three-vertical')} />
+                </div>
             </div>
-            <ul className='menu_list'>
-                <li className='list_item'><a className='item_link'>menu item 1</a></li>
-                <li className='list_item'><a className='item_link'>menu item 2</a></li>
-                <li className='list_item'><a className='item_link'>menu item 3</a></li>
-            </ul>
-            <div className='menu_profile'>
-                <h4>Pospigos</h4>
-                <div className='profile_image' />
-                <span className='icon-dots-three-vertical' />
-            </div>
-        </div>
-    </header>
-)
+        </header>
+    )
+}
 
 export default Header
