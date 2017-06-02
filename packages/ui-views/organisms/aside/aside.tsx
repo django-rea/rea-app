3164a0e20db3a045462c6da7166fdfcb3f7cba78
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-02 15:34:27 +0200

    implemented themeable aside component

--- packages/ui-views/organisms/aside/aside.tsx
+++ packages/ui-views/organisms/aside/aside.tsx
@@ -1,35 +1,44 @@
 import * as React from 'react'
+import * as themeable from 'react-themeable'
 import { SFC } from 'react'
 
-const Aside: SFC<{}> = () => (
-  <aside id='aside'>
-    <span className='aside_logo' />
-    <ul className='aside_list'>
-        <li className='list_item'>
-            <a className='item_link'>
-                <span className='link_image' />
-            </a>
-        </li>
-        <li className='list_item active'>
-            <a className='item_link'>
-                <span className='link_image' />
-            </a>
-        </li>
-        <li className='list_item'>
-            <a className='item_link'>
-                <span className='link_image' />
-            </a>
-        </li>
-        <li className='list_item'>
-            <a className='item_link'>
-                <span className='link_image' />
-            </a>
-        </li>
-    </ul>
-    <div className='aside_bottom'>
-        <span className='icon-plus' />
-    </div>
-  </aside>
-)
+interface Props {
+  theme: Object
+}
+
+const Aside: SFC<Props> = ({ theme }) => {
+  let currentTheme = themeable(theme)
+  console.log(this)
+  return (
+      <aside {...currentTheme(1, 'aside')} >
+        <span {...currentTheme(2, 'aside_logo')} />
+        <ul {...currentTheme(3, 'aside_list')} >
+            <li {...currentTheme(4, 'list_item')} >
+                <a {...currentTheme(5, 'item_link')} >
+                    <span {...currentTheme(6, 'link_image')} />
+                </a>
+            </li>
+            <li {...currentTheme(7, 'list_item active')}>
+                <a {...currentTheme(8, 'item_link')} >
+                    <span {...currentTheme(9, 'link_image')} />
+                </a>
+            </li>
+            <li {...currentTheme(10, 'list_item')} >
+                <a {...currentTheme(11, 'item_link')} >
+                    <span {...currentTheme(12, 'link_image')} />
+                </a>
+            </li>
+            <li {...currentTheme(13, 'list_item')} >
+                <a {...currentTheme(14, 'item_link')} >
+                    <span {...currentTheme(15, 'link_image')} />
+                </a>
+            </li>
+        </ul>
+        <div {...currentTheme(16, 'aside_bottom')} >
+            <span {...currentTheme(17, 'icon-plus')} />
+        </div>
+    </aside>
+  )
+}
 
 export default Aside
