29dee59f0d396e27ccfe8cd80725a023fc33113b
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-05 10:07:10 +0200

    members page template

---
+++ packages/app/pages/Members/index.tsx
@@ -0,0 +1,19 @@
+/**
+ * Members page
+ *
+ * @package: REA app
+ * @author:  ivan <bernini@inventati.org>
+ * @since:   2017-04-06
+ */
+
+import * as React from 'react'
+import ProjectTemplate from '@vflows/views/templates/ProjectTemplate'
+
+import Sidebar from '@vflows/views/organisms/Sidebar'
+import MembersBig from '@vflows/views/organisms/MembersBig'
+
+const MembersPage = () => (
+    <MembersBig />
+)
+
+export default MembersPage
