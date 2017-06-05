9161391b00a20de16cb43be45ee65abb9be12a32
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-05 10:06:59 +0200

    added contributions page template

---
+++ packages/app/pages/Contributions/index.tsx
@@ -0,0 +1,19 @@
+/**
+ * Contributions page
+ *
+ * @package: REA app
+ * @author:  ivan <bernini@inventati.org>
+ * @since:   2017-04-06
+ */
+
+import * as React from 'react'
+
+import List from '@vflows/views/organisms/List'
+
+import ProjectTemplate from '@vflows/views/templates/ProjectTemplate'
+
+const ContributionsPage = () => (
+    <List />
+)
+
+export default ContributionsPage
