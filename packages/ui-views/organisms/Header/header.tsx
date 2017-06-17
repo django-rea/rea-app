e5f178b62508299ec92291339fce9672fb073425
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-17 21:31:25 +0200

    fetch current user on header

--- packages/ui-views/organisms/Header/header.tsx
+++ packages/ui-views/organisms/Header/header.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react'
 import * as themeable from 'react-themeable'
 import { SFC } from 'react'
+import CurrentUser from '@vflows/bindings/user/CurrentUser'
+
+interface UserProps {
+  data?: {
+    myAgent: {
+      name: string, // :TODO: create custom HoC to help prehandle this output
+      image: string
+    },
+  },
+  loading?: boolean,
+  error?: Error,
+  theme: Object
+}
 
 interface State {
   action: boolean,
@@ -10,9 +23,20 @@ interface Props {
   theme: Object
 }
 
+/* eslint no-nested-ternary: 0 */
+const UsernameDisplay: SFC<UserProps> = CurrentUser(({ data, loading, error, theme }) => {
+  return (
+    loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error</p> :
+    <div {...theme(14, 'menu_profile')} >
+        <h4>{data ? data.myAgent.name : 'nobody'}</h4>
+        <div {...theme(15, 'profile_image')}  />
+        <span {...theme(16, 'icon-dots-three-vertical')} />
+    </div>
+  ))
+})
+
 const Header: SFC<Props> = ({ theme }) => {
     let currentTheme = themeable(theme)
-    console.log(currentTheme)
     return (
         <header {...currentTheme(1, 'main_header')} >
             <h2  {...currentTheme(2, 'header_title')}>FreedomCoop</h2>
@@ -26,11 +50,7 @@ const Header: SFC<Props> = ({ theme }) => {
                     <li {...currentTheme(10, 'list_item')} ><a {...currentTheme(11, 'item_link')} >menu item 2</a></li>
                     <li {...currentTheme(12, 'list_item')} ><a {...currentTheme(13, 'item_link')} >menu item 3</a></li>
                 </ul>
-                <div {...currentTheme(14, 'menu_profile')} >
-                    <h4>Pospigos</h4>
-                    <div {...currentTheme(15, 'profile_image')}  />
-                    <span {...currentTheme(16, 'icon-dots-three-vertical')} />
-                </div>
+                <UsernameDisplay theme={currentTheme} />
             </div>
         </header>
     )
