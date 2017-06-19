bd6d5e8d4fb76a98dc0bef3e771956d5d48c3889
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-19 22:44:07 +0200

    fix activeClass on navigation menu

--- packages/ui-views/organisms/SecondaryMenu/secondaryMenu.tsx
+++ packages/ui-views/organisms/SecondaryMenu/secondaryMenu.tsx
@@ -1,45 +1,44 @@
 import * as React from 'react'
 import * as themeable from 'react-themeable'
 import { SFC } from 'react'
-import {Link} from 'react-router'
+import {withRouter, Link} from 'react-router'
 
 interface Props {
   theme: Object
 }
 
 
-const SecondaryMenu: SFC<Props> = ({ theme }) => {
+const SecondaryMenu: SFC<Props> = ({ router, theme }) => {
   let currentTheme = themeable(theme)
     return (
       <section {...currentTheme(0, 'secondaryMenu')}>
         <ul {...currentTheme(1, 'menu_list')} >
-            <li {...currentTheme(2, 'list_item', 'active')} >
-                <Link to={'projects/test'} {...currentTheme(3, 'item_link')} >
+            <li {...currentTheme(2, 'list_item', router.isActive('projects/test', true) && 'active')} >
+                <Link activeClassName={'active'} to={'projects/test'} {...currentTheme(3, 'item_link')} >
                     Overview
                 </Link>
             </li>
-            <li {...currentTheme(4, 'list_item')} >
-                <Link to={'projects/test/processes'} {...currentTheme(5, 'item_link')} >
+            <li {...currentTheme(4, 'list_item' , router.isActive('projects/test/processes', true) && 'active')} >
+                <Link activeClassName={'active'} to={'projects/test/processes'} {...currentTheme(5, 'item_link')} >
                     Processes
                 </Link>
             </li>
-            <li {...currentTheme(6, 'list_item')} >
-                <Link to={'projects/test/contributions'} {...currentTheme(7, 'item_link')} >
+            <li {...currentTheme(6, 'list_item', router.isActive('projects/test/contributions', true) && 'active')} >
+                <Link activeClassName={'active'} to={'projects/test/contributions'} {...currentTheme(7, 'item_link')} >
                     Contributions
                 </Link>
             </li>
-            <li {...currentTheme(8, 'list_item')} >
-                <Link to={'projects/test/members'} {...currentTheme(9, 'item_link')} >
+            <li {...currentTheme(8, 'list_item', router.isActive('projects/test/members', true) && 'active')} >
+                <Link activeClassName={'active'} to={'projects/test/members'} {...currentTheme(9, 'item_link')} >
                     Members
                 </Link>
             </li>
         </ul>    
-        <div {...currentTheme(8, 'menu_actions')} >
+        <div {...currentTheme(8, 'menu_actions', router.isActive('projects/test', true) && 'active')} >
             <button {...currentTheme(8, 'actions_process')} >Create new process</button>
         </div>
       </section>
     )
   }
-}
 
-export default SecondaryMenu
+export default withRouter(SecondaryMenu)
