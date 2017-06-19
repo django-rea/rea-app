8879d7a5cb4fcdaa591094f81b7fb0edbcede015
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-19 23:05:59 +0200

    query the members list on overview page

--- packages/ui-views/organisms/Members/members.tsx
+++ packages/ui-views/organisms/Members/members.tsx
@@ -1,57 +1,52 @@
 import * as React from 'react'
 import * as themeable from 'react-themeable'
 import { SFC } from 'react'
+import { authedGraphQL } from '../../../services/api'
 
-interface Props {
+interface MembersProps {
+  data?: {
+    agent: {
+      members: any // :TODO: create custom HoC to help prehandle this output
+    },
+  },
+  loading?: boolean,
+  error?: Error,
   theme: Object
 }
 
-const Members: SFC<Props>  = ({ theme }) => {
+
+const MembersQuery = authedGraphQL(`
+  agent(id: 135) {
+    members {
+      name
+      id
+      image
+    }
+  }
+`)
+
+const Members: SFC<MembersProps>  = MembersQuery(({data, loading, error, theme }) => {
     let currentTheme = themeable(theme)
+    console.log(data)
+    console.log(error)
     return (
+        loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error</p> :
         <section {...currentTheme(0, 'members')}>
         <h4 {...currentTheme(1, 'members_title')}>Members</h4>    
         <ul {...currentTheme(2, 'members_list')}>
-            <li {...currentTheme(3, 'list_item')}>
-                <a {...currentTheme(4, 'item_link')}>
-                    <span {...currentTheme(5, 'link_image')}></span>
+            {data.agent.members.map((item, i) => (
+            <li {...currentTheme(i+i+i+3, 'list_item')}>
+                <a {...currentTheme(i+i+i+4, 'item_link')}>
+                    <span {...currentTheme(i+i+i+5, 'link_image')}>
+                        <img src={item.image} />
+                    </span>
                 </a>
             </li>
-            <li {...currentTheme(6, 'list_item')}>
-                <a {...currentTheme(7, 'item_link')}>
-                        <span {...currentTheme(8, 'link_image')}></span>
-                </a>
-            </li>
-            <li {...currentTheme(9, 'list_item')}>
-                <a {...currentTheme(10, 'item_link')}>
-                    <span {...currentTheme(11, 'link_image')}></span>
-                </a>
-            </li>
-            <li {...currentTheme(12, 'list_item')}>
-                <a {...currentTheme(13, 'item_link')}>
-                    <span {...currentTheme(14, 'link_image')}></span>
-                </a>
-            </li>
-            <li {...currentTheme(15, 'list_item')}>
-                <a {...currentTheme(16, 'item_link')}>
-                    <span {...currentTheme(17, 'link_image')}></span>
-                </a>
-            </li>
-            <li {...currentTheme(18, 'list_item')}>
-                <a {...currentTheme(19, 'item_link')}>
-                    <span {...currentTheme(20, 'link_image')}></span>
-                </a>
-            </li>
-            <li {...currentTheme(21, 'list_item')}>
-                <a {...currentTheme(22, 'item_link')}>
-                    <span {...currentTheme(23, 'link_image')}></span>
-                </a>
-            </li>
-
+            )}
         </ul>
         <a {...currentTheme(24, 'members_link')}>See All</a>
         </section>
     )
-}
+})
 
 export default Members
