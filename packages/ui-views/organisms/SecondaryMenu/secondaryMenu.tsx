fc8a1bb5a2b3cc468200a1595ded51440150faa0
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-21 17:27:00 +0200

    correct routes link

--- packages/ui-views/organisms/SecondaryMenu/secondaryMenu.tsx
+++ packages/ui-views/organisms/SecondaryMenu/secondaryMenu.tsx
@@ -8,33 +8,33 @@ interface Props {
 }
 
 
-const SecondaryMenu: SFC<Props> = ({ router, theme }) => {
+const SecondaryMenu: SFC<Props> = ({ id, router, theme }) => {
   let currentTheme = themeable(theme)
     return (
       <section {...currentTheme(0, 'secondaryMenu')}>
         <ul {...currentTheme(1, 'menu_list')} >
-            <li {...currentTheme(2, 'list_item', router.isActive('projects/test', true) && 'active')} >
-                <Link to={'projects/test'} {...currentTheme(3, 'item_link')} >
+            <li {...currentTheme(2, 'list_item', router.isActive(`projects/${id}`, true) && 'active')} >
+                <Link to={`projects/${id}`} {...currentTheme(3, 'item_link')} >
                     Overview
                 </Link>
             </li>
-            <li {...currentTheme(4, 'list_item' , router.isActive('projects/test/processes', true) && 'active')} >
-                <Link to={'projects/test/processes'} {...currentTheme(5, 'item_link')} >
+            <li {...currentTheme(4, 'list_item' , router.isActive(`projects/${id}/processes`, true) && 'active')} >
+                <Link to={`projects/${id}/processes`} {...currentTheme(5, 'item_link')} >
                     Processes
                 </Link>
             </li>
-            <li {...currentTheme(6, 'list_item', router.isActive('projects/test/contributions', true) && 'active')} >
-                <Link to={'projects/test/contributions'} {...currentTheme(7, 'item_link')} >
+            <li {...currentTheme(6, 'list_item', router.isActive(`projects/${id}/contributions`, true) && 'active')} >
+                <Link to={`projects/${id}/contributions`} {...currentTheme(7, 'item_link')} >
                     Contributions
                 </Link>
             </li>
-            <li {...currentTheme(8, 'list_item', router.isActive('projects/test/members', true) && 'active')} >
-                <Link to={'projects/test/members'} {...currentTheme(9, 'item_link')} >
+            <li {...currentTheme(8, 'list_item', router.isActive(`projects/${id}/members`, true) && 'active')} >
+                <Link to={`projects/${id}/members`} {...currentTheme(9, 'item_link')} >
                     Members
                 </Link>
             </li>
         </ul>    
-        <div {...currentTheme(8, 'menu_actions', router.isActive('projects/test', true) && 'active')} >
+        <div {...currentTheme(8, 'menu_actions', router.isActive(`projects/${id}`, true) && 'active')} >
             <button {...currentTheme(8, 'actions_process')} >Create new process</button>
         </div>
       </section>
