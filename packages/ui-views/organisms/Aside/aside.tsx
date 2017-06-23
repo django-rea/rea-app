bf4ba6a963db78cb7a0400355efd65193fd2d8e5
pospi <pospi@spadgos.com>
2017-06-23 22:14:56 +0700

    bind aside to new orgs query data and fix theme

--- packages/ui-views/organisms/Aside/aside.tsx
+++ packages/ui-views/organisms/Aside/aside.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react'
 import * as themeable from 'react-themeable'
-import { SFC } from 'react'
-import {Link, withRouter} from 'react-router'
-import { authedGraphQL } from '../../../services/api'
+import { withRouter } from 'react-router'
+
+import BindOrgs from '@vflows/bindings/agent/AgentOrganizations'
+import { AgentType } from '@vflows/bindings/agent/agent'
+
+import Link from '../../atoms/Link'
 
 interface OrgsProps {
-  data?: {
-    myAgent: {
-      organizations: any // :TODO: create custom HoC to help prehandle this output
-    },
-  },
+  organizations: Array<AgentType>,
   loading?: boolean,
   error?: Error,
   theme: Object,
@@ -18,46 +17,38 @@ interface OrgsProps {
 
 interface Props {
   theme: Object,
-  router: Object
+  router: {
+    params: {
+      id: string,
+    },
+  },
 }
 
-
-const asideQuery = authedGraphQL(`
-  myAgent {
-     id 
-     organizations {
-        id
-        name
-        image
-      }
-  }
-`)
-
 /* eslint no-nested-ternary: 0 */
-const OrgsList: SFC<OrgsProps> = asideQuery(({ data, loading, error, theme, router }) => {
+const OrgsList = BindOrgs(({ organizations, loading, error, theme, router }: OrgsProps) => {
+  let currentTheme = themeable(theme)
   return (
-    loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error</p> :
-   <ul {...theme(30000, 'aside_list')} >
-       {data.myAgent.organizations.map((item, i) => (
-        <li {...theme(i + i + i + 1, 'list_item', router.isActive('projects/' + item.id, true) && 'active')} >
-            <Link to={'projects/' + item.id} {...theme(i + i + i + 2, 'item_link')} >
-                <span {...theme( i + i + i + 3, 'link_image')}>
+    loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error {error.message}</p> : (
+   <ul {...currentTheme(30000, 'aside_list')} >
+       {organizations.map((item, i) => (
+        <li {...currentTheme(i + i + i + 1, 'list_item', router.isActive('projects/' + item.id, true) && 'active')} >
+            <Link to={'projects/' + item.id} {...currentTheme(i + i + i + 2, 'item_link')} >
+                <span {...currentTheme( i + i + i + 3, 'link_image')}>
                     <img src={item.image} />
                 </span>
             </Link>
         </li>
        ))}
     </ul>
-  ))
+  )))
 })
 
-
-const Aside: SFC<Props> = ({ router, theme }) => {
+const Aside = ({ router, theme }: Props) => {
   let currentTheme = themeable(theme)
   return (
       <aside {...currentTheme(1000, 'aside')} >
         <span {...currentTheme(2000, 'aside_logo')} />
-        <OrgsList router={router} theme={currentTheme} />
+        <OrgsList router={router} theme={currentTheme} agentId={router.params.id} />
         <div {...currentTheme(16, 'aside_bottom')} >
             <span {...currentTheme(17, 'icon-plus')} />
         </div>
