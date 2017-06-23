a12d0f976edb1425db1df9b2a63c0f1975954175
pospi <pospi@spadgos.com>
2017-06-23 22:15:28 +0700

    bind organisation members to new data

--- packages/ui-views/organisms/MembersBig/MembersBig.tsx
+++ packages/ui-views/organisms/MembersBig/MembersBig.tsx
@@ -1,51 +1,38 @@
 import * as React from 'react'
 import * as themeable from 'react-themeable'
-import { SFC } from 'react'
-import { authedGraphQL } from '../../../services/api'
+
+import BindMembers from '@vflows/bindings/agent/AgentMembers'
+import { AgentType } from '@vflows/bindings/agent/agent'
 
 interface MembersProps {
-  data?: {
-    agent: {
-      members: any // :TODO: create custom HoC to help prehandle this output
-    },
-  },
+  members: Array<AgentType>, // :TODO: create custom HoC to help prehandle this output
   loading?: boolean,
   error?: Error,
   theme: Object
 }
 
-
-const MembersQuery = authedGraphQL(`
-  agent(id: 135) {
-    members {
-      name
-      image
-    }
-  }
-`)
-
-const MembersBig: SFC<MembersProps> = MembersQuery(({data, theme, loading, error }) => {
+const MembersBig = BindMembers(({ members, theme, loading, error }: MembersProps) => {
     let currentTheme = themeable(theme)
     return (
-      loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error</p> :
+      loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error</p> : (
       <section {...currentTheme(1, 'membersBig')}>
-        <h4 {...currentTheme(2, 'membersBig_title')}>{data.agent.members.length} Members</h4>
+        <h4 {...currentTheme(2, 'membersBig_title')}>{members.length} Members</h4>
         <div {...currentTheme(3, 'membersBig_list')}>
           <div {...currentTheme(4, 'row')}>
-            {data.agent.members.map((item, i) => (
+            {members.map((item, i) => (
             <div {...currentTheme(i + i + i + i + 5, 'medium-2', 'columns', 'end')}>
               <div {...currentTheme(i + i + i + i + 6, 'list_member')}>
                 <span {...currentTheme(i + i + i + i + 7, 'member_photo')}>
                   <img src={item.image} />
                 </span>
                 <h3 {...currentTheme(i + i + i + i + 8, 'member_name')}>{item.name}</h3>
-              </div>  
+              </div>
             </div>
             ))}
           </div>
         </div>
       </section>
-    )
+    ))
   }
 })
 
