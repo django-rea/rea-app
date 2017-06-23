622e3b33fa8a2b64a6c9a26058b5f45e0f924f15
pospi <pospi@spadgos.com>
2017-06-23 22:05:48 +0700

    bind agent data to agent overview page

--- packages/app/pages/Overview/index.tsx
+++ packages/app/pages/Overview/index.tsx
@@ -15,23 +15,30 @@ import Sidebar from '@vflows/views/organisms/Sidebar'
 import Overview from '@vflows/views/organisms/Overview'
 import Members from '@vflows/views/organisms/Members'
 import List from '@vflows/views/organisms/List'
-import Agent from '@vflows/bindings/agent/agent'
+import BindAgent, { AgentType } from '@vflows/bindings/agent/agent'
 
-const OverviewPage = Agent(({data, loading, error}) => {
+interface Props {
+  agent?: AgentType,
+  loading?: boolean,
+  error?: Error,
+}
+
+const OverviewPage = BindAgent(({ agent, loading, error }: Props) => {
   return (
     loading ? <strong>Loading...</strong> : (
-    error ? <p style={{ color: '#F00' }}>API error</p> :
+    error ? <p style={{ color: '#F00' }}>API error</p> : (
     <div>
       <Overview
-        note={data.agent.note}
-        processes={data.agent.unfinishedProcesses}
-        inventory={data.agent.ownedEconomicResources}
-        members={data.agent.members}
+        note={agent.note}
+        processes={agent.agentProcesses}
+        inventory={agent.ownedEconomicResources}
+        members={agent.members}
       />
-      <Members members={data.agent.members}/>
+      <Members members={agent.members}/>
       <List />
     </div>)
     )
+  )
 })
 
 export default OverviewPage
