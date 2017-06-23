a3b187dfbdd7557e4d84a446f3b1ebecf2d1ec9f
pospi <pospi@spadgos.com>
2017-06-23 22:56:32 +0700

    change aside binding to only load user's orgs, and highlight active org based on URL rather than loading different data

--- packages/ui-views/organisms/Aside/aside.tsx
+++ packages/ui-views/organisms/Aside/aside.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import * as themeable from 'react-themeable'
 import { withRouter } from 'react-router'
 
-import BindOrgs from '@vflows/bindings/agent/AgentOrganizations'
+import BindOrgs from '@vflows/bindings/user/CurrentOrganizations'
 import { AgentType } from '@vflows/bindings/agent/agent'
 
 import Link from '../../atoms/Link'
@@ -12,11 +12,6 @@ interface OrgsProps {
   loading?: boolean,
   error?: Error,
   theme: Object,
-  router: Object
-}
-
-interface Props {
-  theme: Object,
   router: {
     params: {
       id: string,
@@ -24,15 +19,19 @@ interface Props {
   },
 }
 
+interface Props {
+  theme: Object,
+}
+
 /* eslint no-nested-ternary: 0 */
-const OrgsList = BindOrgs(({ organizations, loading, error, theme, router }: OrgsProps) => {
+const OrgsList = BindOrgs(withRouter(({ organizations, loading, error, theme, router }: OrgsProps) => {
   let currentTheme = themeable(theme)
   return (
     loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error {error.message}</p> : (
    <ul {...currentTheme(30000, 'aside_list')} >
        {organizations.map((item, i) => (
         <li {...currentTheme(i + i + i + 1, 'list_item', router.isActive('projects/' + item.id, true) && 'active')} >
-            <Link to={'projects/' + item.id} {...currentTheme(i + i + i + 2, 'item_link')} >
+            <Link href={'projects/' + item.id} {...currentTheme(i + i + i + 2, 'item_link')} >
                 <span {...currentTheme( i + i + i + 3, 'link_image')}>
                     <img src={item.image} />
                 </span>
@@ -41,14 +40,14 @@ const OrgsList = BindOrgs(({ organizations, loading, error, theme, router }: Org
        ))}
     </ul>
   )))
-})
+}))
 
-const Aside = ({ router, theme }: Props) => {
+const Aside = ({ theme }: Props) => {
   let currentTheme = themeable(theme)
   return (
       <aside {...currentTheme(1000, 'aside')} >
         <span {...currentTheme(2000, 'aside_logo')} />
-        <OrgsList router={router} theme={currentTheme} agentId={router.params.id} />
+        <OrgsList theme={theme} />
         <div {...currentTheme(16, 'aside_bottom')} >
             <span {...currentTheme(17, 'icon-plus')} />
         </div>
@@ -56,4 +55,4 @@ const Aside = ({ router, theme }: Props) => {
   )
 }
 
-export default withRouter(Aside)
+export default Aside
