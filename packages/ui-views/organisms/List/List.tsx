8ff8f1aaeff2c1ca7c82a7ed2af8ebe172cea4ab
ivan minutillo <minutillo.ivan@gmail.com>
2017-06-27 11:05:38 +0200

    update list component with new layout and props

--- packages/ui-views/organisms/List/List.tsx
+++ packages/ui-views/organisms/List/List.tsx
@@ -3,44 +3,28 @@ import * as themeable from 'react-themeable'
 import { SFC } from 'react'
 
 interface Props {
-  theme: Object
+  theme: Object,
+  events: Array<Object>
 }
 
-const List: SFC<Props> = ({ theme }) => {
+const List: SFC<Props> = ({ theme, events }) => {
     let currentTheme = themeable(theme)
     return (
-      <section {...currentTheme(1, 'list')}>
+      <section {...currentTheme(1, 'list_spaced')}>
         <div {...currentTheme(2, 'list_info')}>
             <h4 {...currentTheme(3, 'info_title')}>Latest contributions</h4>
         </div>
-        <div {...currentTheme(4, 'list_item')}>
-            <div {...currentTheme(5, 'item_info')}>
-                <h3 {...currentTheme(6, 'info_name')}>LynnFhoster</h3>
-                <h2 {...currentTheme(7, 'info_title')}>Process title blabla</h2>
-                <div {...currentTheme(8, 'info_description')}>lorem ipsum bablabla lorem ipsum bablabla lorem ipsum bablabla</div>
-                <div {...currentTheme(9, 'info_actions')}>
-                    <span {...currentTheme(10, 'actions_tasks')}>6 tasks</span>
-                    <span {...currentTheme(11, 'actions_date')}>5 Jun 2017</span>
-                </div>
-                <div {...currentTheme(12, 'item_actions')}>
-                    <button {...currentTheme(13, 'actions_open')}>open</button>
-                    <span />
-                </div>
-            </div>
-            <div {...currentTheme(14, 'item_info')}>
-                <h3 {...currentTheme(15, 'info_name')}>LynnFhoster</h3>
-                <h2 {...currentTheme(16, 'info_title')}>Process title blabla</h2>
-                <div {...currentTheme(17, 'info_description')}>lorem ipsum bablabla lorem ipsum bablabla lorem ipsum bablabla</div>
-                <div {...currentTheme(18, 'info_actions')}>
-                    <span {...currentTheme(19, 'actions_tasks')}>6 tasks</span>
-                    <span {...currentTheme(20, 'actions_date')}>5 Jun 2017</span>
-                </div>
-                <div {...currentTheme(21, 'item_actions')}>
-                    <button {...currentTheme(22, 'actions_open')}>open</button>
-                    <span />
-                </div>
-            </div>
-        </div>
+        {events.map((item, i) => (
+          <div {...currentTheme(i+i+i+i+i+i+4, 'list_item')}>
+              <div {...currentTheme(i+i+i+i+i+i+5, 'item_info')}>
+                  <h2 {...currentTheme(i+i+i+i+i+i+6, 'info_title')}><a>{item.provider.name}</a> {item.action} {item.numericValue} {item.unit} from/to <a>{item.receiver.name}</a></h2>
+                  <div {...currentTheme(i+i+i+i+i+i+8, 'info_actions')}>
+                      <button {...currentTheme(i+i+i+i+i+i+9, 'actions_date')}>{item.start}</button>
+                  </div>
+                  <div {...currentTheme(i+i+i+i+i+i+7, 'info_description')}>{item.note}</div>
+              </div>
+          </div>
+        ))}
       </section>
     )
 }
