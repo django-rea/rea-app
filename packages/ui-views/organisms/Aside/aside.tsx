ccb375f27c25cce0c790a30d00947e6f12ad847d
ivan minutillo <minutillo.ivan@gmail.com>
2017-07-05 23:25:21 +0200

    update sidebar section

--- packages/ui-views/organisms/Aside/aside.tsx
+++ packages/ui-views/organisms/Aside/aside.tsx
@@ -29,7 +29,7 @@ const OrgsList = BindOrgs(withRouter(({ organizations, loading, error, theme, ro
   return (
     loading ? <strong>Loading...</strong> : (error ? <p style={{ color: '#F00' }}>API error {error.message}</p> : (
    <ul {...currentTheme(30000, 'aside_list')} >
-      <h4 {...currentTheme(30001, 'list_title')}>Organizations</h4>
+      <h4 {...currentTheme(30001, 'list_title')}>organizations</h4>
        {organizations.map((item, i) => (
         <li {...currentTheme(i + i + i + 1, 'list_item', router.isActive('projects/' + item.id, true) && 'active')} >
             <Link href={'projects/' + item.id} {...currentTheme(i + i + i + 2, 'item_link')} >
