357791a443e845dfd1089f0db423a6a8ce42bc1b
ivan minutillo <minutillo.ivan@gmail.com>
2017-07-06 14:53:48 +0200

    update aside style and behaviour

--- packages/ui-views/organisms/Aside/aside.tsx
+++ packages/ui-views/organisms/Aside/aside.tsx
@@ -31,7 +31,7 @@ const OrgsList = BindOrgs(withRouter(({ organizations, loading, error, theme, ro
    <ul {...currentTheme(30000, 'aside_list')} >
       <h4 {...currentTheme(30001, 'list_title')}>organizations</h4>
        {organizations.map((item, i) => (
-        <li {...currentTheme(i + i + i + 1, 'list_item', router.isActive('projects/' + item.id, true) && 'active')} >
+        <li {...currentTheme(i + i + i + 1, 'list_item', router.isActive('projects/' + item.id) && 'active')} >
             <Link href={'projects/' + item.id} {...currentTheme(i + i + i + 2, 'item_link')} >
                 <span {...currentTheme( i + i + i + 3, 'link_image')}>
                     <img src={item.image} />
