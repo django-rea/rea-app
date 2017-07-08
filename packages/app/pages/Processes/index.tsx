902430107f2e7111dc6d66a684ece7162e98c44c
ivan minutillo <minutillo.ivan@gmail.com>
2017-07-08 15:54:51 +0200

    show basic info on processes list

--- packages/app/pages/Processes/index.tsx
+++ packages/app/pages/Processes/index.tsx
@@ -1,25 +1,5 @@
-/**
- * Processes page
- *
- * @package: REA app
- * @author:  ivan <bernini@inventati.org>
- * @since:   2017-05-06
- */
+import Component from './processes'
+import themed from '@vflows/views/themed'
+const styles = require('./index.scss')
 
-import * as React from 'react'
-import { AgentType } from '@vflows/bindings/agent/agent'
-
-interface Props {
-  agent?: AgentType,
-  router: {
-    params: {
-      id: string,
-    },
-  },
-}
-
-const ProcessesPage = ({ agent, router }: Props) => (
-    <div>processi</div>
-)
-
-export default ProcessesPage
+export default themed(Component, styles)
