18e0af8d80976f3f7bf4e6b9caf3adcd94a92cae
ivan minutillo <minutillo.ivan@gmail.com>
2017-07-09 16:43:30 +0200

    header

--- packages/ui-views/organisms/Header/header.tsx
+++ packages/ui-views/organisms/Header/header.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import * as themeable from 'react-themeable'
 import { SFC } from 'react'
 import CurrentUser from '@vflows/bindings/user/CurrentUser'
+import Link from '../../atoms/Link'
+import {Bell, Search, Horizontal} from '../../icons'
 
 interface UserProps {
   user?: {
@@ -22,13 +24,17 @@ const Header = CurrentUser(({ user, loading, error, theme }: UserProps) => {
             <span  {...currentTheme(2, 'header_logo')} />
             <div {...currentTheme(4, 'header_search')} >
               <input {...currentTheme(5, 'search', 'input')} placeholder='Search on kamasi' />
-              <span {...currentTheme(6, 'icon', 'icon-magnifying-glass')} />
+              <span {...currentTheme(6, 'search_icon')}><Search /></span>
             </div>
+            <Link href={'/projects'}>All Projects</Link>
             <div {...currentTheme(3, 'header_menu')} >
+                  <div {...currentTheme(17, 'menu_bell')}>
+                    <Bell />
+                  </div>
                   <div {...currentTheme(14, 'menu_profile')} >
                     <div {...currentTheme(15, 'profile_image')}  />
                     <h4>{user.name || 'nobody'}</h4>
-                    <span {...currentTheme(16, 'icon-dots-three-horizontal')} />
+                    <span {...currentTheme(16, 'profile_other')}><Horizontal /></span>
                 </div>
             </div>
           </div>
