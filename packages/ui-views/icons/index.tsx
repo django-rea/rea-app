bc3fd841e12949775c5fbf0d8b768883271086ee
ivan minutillo <minutillo.ivan@gmail.com>
2017-07-09 16:43:03 +0200

    basic icons set

--- packages/ui-views/icons/index.tsx
+++ packages/ui-views/icons/index.tsx
@@ -16,8 +16,17 @@ const nullServerComponent = () => (<span />)
 
 export const FreedomCoopLogo = !browser ? nullServerComponent : require('./FC-logo-only.svg').default
 
-export const Menu = !browser ? nullServerComponent : require('material-design-icons/navigation/svg/production/ic_menu_24px.svg').default
+export const Menu = !browser ? nullServerComponent : require('./menu.svg').default
+export const Search = !browser ? nullServerComponent : require('./search.svg').default
+export const Up = !browser ? nullServerComponent : require('./up.svg').default
+export const Down = !browser ? nullServerComponent : require('./down.svg').default
+export const Calendar = !browser ? nullServerComponent : require('./calendar.svg').default
+export const Edit = !browser ? nullServerComponent : require('./edit.svg').default
+export const Bell = !browser ? nullServerComponent : require('./bell.svg').default
+export const Horizontal = !browser ? nullServerComponent : require('./more-horizontal.svg').default
+export const Vertical = !browser ? nullServerComponent : require('./more-vertical.svg').default
 // Checkbox: require('material-design-icons/toggle/svg/production/ic_check_box_24px.svg').default
+
 export const Checkbox = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_check_circle_24px.svg').default
 export const Exit = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_exit_to_app_24px.svg').default
 
@@ -27,3 +36,4 @@ export const Group = !browser ? nullServerComponent : require('material-design-i
 export const GroupAdd = !browser ? nullServerComponent : require('material-design-icons/social/svg/production/ic_group_add_24px.svg').default
 
 export const Bank = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_account_balance_24px.svg').default
+export const Icon = !browser ? nullServerComponent : require('material-design-icons/action/svg/production/ic_account_balance_24px.svg').default
