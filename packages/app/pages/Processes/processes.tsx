0dd14b1900ce645cc6dbefe82dc1ab4faeb811d6
ivan minutillo <minutillo.ivan@gmail.com>
2017-07-26 12:57:17 +0200

    edit processes list

--- packages/app/pages/Processes/processes.tsx
+++ packages/app/pages/Processes/processes.tsx
@@ -21,27 +21,32 @@ interface Props {
   },
 }
 
-const ProcessesPage = ({ agent, router, theme }: Props) => {
+const ProcessesPage = ({ agent, router, theme, handleOpenModal }: Props) => {
   let currentTheme = themeable(theme)
   return (
     <section  {...currentTheme(1, 'processes_list')}>
       {agent.agentProcesses.map((process, i) => (
-        <div {...currentTheme((i * 12) + 2, 'processes_item')}>
+        <div {...currentTheme((i * 12) + 2, 'processes_item')} onClick={()=>handleOpenModal(process.id)}>
+          {process.isStarted ? <o {...currentTheme((i*12)+1000, 'item_green'} ></o> : '' }
           <div {...currentTheme((i * 12) + 3, 'item_first')}>
             <h3 {...currentTheme((i * 12) + 4, 'first_title')}>{process.name}</h3>
-            <div {...currentTheme((i * 12) + 5, 'first_actions' )}>
-              <span {...currentTheme((i * 12) + 6, 'action_more' )}><Horizontal /></span>
-              <span {...currentTheme((i * 12) + 7, 'action_open' )}><Down /></span>
-            </div>
+            {/*<div {...currentTheme((i * 12) + 5, 'first_actions' )}>
+              <span {...currentTheme((i * 12) + 6, 'action_back' )}>1 Prev</span>
+              <span {...currentTheme((i * 12) + 7, 'action_next' )}>0 Next</span>
+            </div>*/}
           </div>
           <div {...currentTheme((i * 12) + 8, 'item_second')}>{process.note}</div>
           <div {...currentTheme((i * 12) + 9, 'item_third')}>
             <span {...currentTheme((i * 12) + 10, 'third_date')}>{process.plannedStart} | {process.plannedDuration}</span>
             <div {...currentTheme((i * 12) + 11, 'third_members')}>
-              <span {...currentTheme((i * 12) + 12, 'members_single')} />
-              <span {...currentTheme((i * 12) + 13, 'members_single')} />
+              {process.workingAgents.map((ag,index)=> (
+                <span {...currentTheme((i * 12) + index + 12, 'members_single')}>
+                  <img src={ag.image} />
+                </span>
+              ))}
             </div>
           </div>
+
         </div>
       ))}
     </section>
