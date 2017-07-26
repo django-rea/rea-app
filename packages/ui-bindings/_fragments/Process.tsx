2912c1aa54158e9ebd4d2988ba8dd81d68732e20
ivan minutillo <minutillo.ivan@gmail.com>
2017-07-26 12:58:26 +0200

    created new apollo fragment for process

---
+++ packages/ui-bindings/_fragments/Process.tsx
@@ -0,0 +1,97 @@
+/**
+ * Query fragments for Economic Agents
+ *
+ * @package: REA app
+ * @author:  pospi <pospi@spadgos.com>
+ * @since:   2017-06-23
+ */
+
+import { gql } from 'react-apollo'
+
+export const coreCommitmentFields = gql`
+fragment coreCommitmentFields on Commitment {
+  action
+  note
+  commitmentStart
+  committedOn
+  due
+  isFinished
+  fulfilledBy {
+    action
+    start
+    note
+    affectedQuantity {
+      numericValue
+      unit {
+        name
+      }
+    }
+    affectedResource {
+      id
+      resourceTaxonomyItem {
+        name
+        category
+      }
+      trackingIdentifier
+    }
+    provider {
+      id
+      name
+      image
+    }
+    receiver {
+      id
+      name
+    }
+  }
+  committedQuantity {
+    numericValue
+    unit {
+      name
+    }
+  }
+  committedTaxonomyItem {
+    name
+    category
+  }
+  provider {
+    id
+    name
+    image
+  }
+  receiver {
+    id
+    name
+  }
+}`
+
+
+export const coreEventFields = gql`
+fragment coreEventFields on EconomicEvent {
+  action
+  start
+  note
+  affectedQuantity {
+    numericValue
+    unit {
+      name
+    }
+  }
+  affectedResource {
+    id
+    resourceTaxonomyItem {
+      name
+      category
+    }
+    trackingIdentifier
+  }
+  provider {
+    id
+    name
+    image
+  }
+  receiver {
+    id
+    name
+  }
+}`
