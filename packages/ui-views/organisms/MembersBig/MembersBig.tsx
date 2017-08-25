74d070b8476be49524b091879f4bd4006ffbcf6a
ivan minutillo <minutillo.ivan@gmail.com>
2017-08-25 19:05:50 +0200

    members divided by typology

--- packages/ui-views/organisms/MembersBig/MembersBig.tsx
+++ packages/ui-views/organisms/MembersBig/MembersBig.tsx
@@ -11,23 +11,28 @@ interface MembersProps {
 
 const MembersBig = ({ members, theme }: MembersProps) => {
     let currentTheme = themeable(theme)
+    let types = members.map(m => m.relationship.category)
+    let unique_types = Array.from(new Set(types))
     return (
       <section {...currentTheme(1, 'membersBig')}>
-        <div {...currentTheme(3, 'membersBig_list')}>
-          <div {...currentTheme(4, 'row')}>
-            {members.map((item, i) => (
-            <div {...currentTheme(i + i + i + i + 5, 'medium-3', 'small-4' 'columns', 'end')}>
-              <div {...currentTheme(i + i + i + i + 6, 'list_member')}>
-                <span {...currentTheme(i + i + i + i + 7, 'member_photo')}>
-                  <img src={item.subject.image} />
-                </span>
-                <h3 {...currentTheme(i + i + i + i + 8, 'member_name')}>{item.subject.name}</h3>
-                <h5>{item.relationship.category}</h5>
+        {unique_types.map((type, i) => (
+          <div {...currentTheme((3 +  i + i +i) + 393, 'membersBig_list')}>
+            <h5 {...currentTheme((2 + i + i + i) + 393, 'list_type')} >{type}</h5>
+            <div {...currentTheme((4 + i + i + i) + 393, 'row')}>
+              {members.filter(item => item.relationship.category === type).map((item, y) => (
+              <div {...currentTheme((y + y + y + y + 5) * 10223, 'medium-3', 'small-4' 'columns', 'end')}>
+                <div {...currentTheme((y + y + y + y + 6) * 10223, 'list_member')}>
+                  <span {...currentTheme((y + y + y + y + 7) * 10223, 'member_photo')}>
+                    <img src={item.subject.image} />
+                  </span>
+                  <h3 {...currentTheme((y + y + y + y + 8) * 10223, 'member_name')}>{item.subject.name}</h3>
+                  <h5>{item.relationship.category}</h5>
+                </div>
               </div>
+              ))}
             </div>
-            ))}
           </div>
-        </div>
+        ))}
       </section>
     )
   }
