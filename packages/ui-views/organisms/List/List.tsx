ba32582756bd97e2e2ba26403a59d733dd5c6e56
ivan minutillo <minutillo.ivan@gmail.com>
2017-09-15 00:11:43 +0200

    recent list updated

--- packages/ui-views/organisms/List/List.tsx
+++ packages/ui-views/organisms/List/List.tsx
@@ -11,20 +11,17 @@ const List: SFC<Props> = ({ theme, events }) => {
     let currentTheme = themeable(theme)
     return (
       <section {...currentTheme(1, 'list_spaced')}>
-        {/*<div {...currentTheme(2, 'list_info')}>
-            <h4 {...currentTheme(3, 'info_title')}>Latest contributions</h4>
-        </div>*/}
+        <div {...currentTheme(2, 'list_info')}>
+            <h4 {...currentTheme(3, 'info_title')}>🔥 Latest contributions</h4>
+        </div>
         <div {...currentTheme(0, 'list_box')}>
         {events.map((item, i) => (
           <div {...currentTheme(i+i+i+i+i+i+4, 'list_item')}>
             <div {...currentTheme(i+i+i+i+i+i+5, 'item_info')}>
             <span {...currentTheme(i+i+i+i+i+i+6, 'item_photo')}><img src={item.provider.image} /></span>
-            <h2 {...currentTheme(i+i+i+i+i+i+7, 'item_title')}><a>{item.provider.name}</a> {item.action} {item.affectedQuantity.numericValue} {item.affectedQuantity.unit ? item.affectedQuantity.unit.name : ''} from/to <a>{item.receiver.name}</a></h2>
+            <h2 {...currentTheme(i+i+i+i+i+i+7, 'item_title')}><a>{item.provider.name}</a> {item.action} {item.affectedQuantity.numericValue} {item.affectedQuantity.unit ? item.affectedQuantity.unit.name : ''} • <b>{item.start}</b> </h2>
             </div>
             <div {...currentTheme(i+i+i+i+i+i+8, 'item_description')}>{item.note}</div>
-            <div {...currentTheme(i+i+i+i+i+i+9, 'item_actions')}>
-                <span {...currentTheme(i+i+i+i+i+i+10, 'actions_date')}>{item.start}</span>
-            </div>
           </div>
         ))}
         </div>
