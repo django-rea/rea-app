5b54d5e74f4094fdf17c0ee526c9cd97349c908d
ivan minutillo <minutillo.ivan@gmail.com>
2017-09-17 11:12:31 +0200

    networks custom query

--- packages/app/pages/Members/index.tsx
+++ packages/app/pages/Members/index.tsx
@@ -9,11 +9,14 @@
 import * as React from 'react'
 import ProjectTemplate from '@vflows/views/templates/ProjectTemplate'
 import { AgentType } from '@vflows/bindings/agent/agent'
-import Sidebar from '@vflows/views/organisms/Sidebar'
 import MembersBig from '@vflows/views/organisms/MembersBig'
+import BindNetwork from '@vflows/bindings/network/network'
 
 interface Props {
   agent?: AgentType,
+  loading: Boolean,
+  error: Object,
+  agentId: Number,
   router: {
     params: {
       id: string,
@@ -21,8 +24,16 @@ interface Props {
   },
 }
 
-const MembersPage = ({ agent, router }: Props) => (
-    <MembersBig members={agent.agentRelationships} agentId={router.params.id} />
-)
+const MembersPage = BindNetwork(({ agent, loading, error, agentId,  router }: Props) => {
+  console.log(agentId)
+  console.log(error)
+  console.log(agent)
+  return (
+  loading ? <strong>Loading...</strong> : (
+    error ? <p style={{ color: '#F00' }}>API error</p> : (
+      <MembersBig members={agent.agentRelationships} agentId={router.params.id} />
+    ))
+  )
+})
 
 export default MembersPage
