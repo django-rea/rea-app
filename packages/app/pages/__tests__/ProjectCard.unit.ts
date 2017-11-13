d117fee1bd1ce40a34d20335b4a75a2d21dceacc
Ryan Guinn <guinnrd@msoe.edu>
2017-11-12 23:37:26 -0600

    ProjectCard test added but failing

--- packages/app/pages/__tests__/ProjectCard.unit.ts
+++ packages/app/pages/__tests__/ProjectCard.unit.ts
@@ -1,9 +1,9 @@
 import React from 'react';
 import ProjectCard from '../AllProjectsPage/AllProjectsPage';
-#import ReactTestUtils from 'react-dom/test-utils';
+//import ReactTestUtils from 'react-dom/test-utils';
 import ShallowRenderer from 'react-test-renderer/shallow';
-#import TestRenderer from 'react-test-renderer';
-#import test from 'tape';
+//import TestRenderer from 'react-test-renderer';
+import * as test from "tape";
 
 const renderer = new ShallowRenderer();
 renderer.render(<ProjectCard />);
@@ -13,3 +13,9 @@ console.log(result);
 console.log(result.type);
 console.log(result.props);
 console.log(result.props.children);
+
+test("Simple math test", (t) => {
+    t.equal(4, 2 + 2);
+    t.true(5 > 2 + 2);
+    t.end();
+});
